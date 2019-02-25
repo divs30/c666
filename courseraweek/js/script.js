29b1dcfb01cfccdbf37f7eb97f3814569529d6ba
divs30 <40079593+divs30@users.noreply.github.com>
2019-02-25 18:12:59 +0530

    modified text file
    modified text file

--- courseraweek/js/script.js
+++ courseraweek/js/script.js
@@ -13,15 +13,13 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
 var dc = {};
 
-var homeHtmlUrl = "snippets/home-snippet.html";
-var allCategoriesUrl =
-  "https://davids-restaurant.herokuapp.com/categories.json";
+var homeHtmlUrl         = "snippets/home-snippet.html";
+var allCategoriesUrl    = "https://davids-restaurant.herokuapp.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
-var categoryHtml = "snippets/category-snippet.html";
-var menuItemsUrl =
-  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
-var menuItemsTitleHtml = "snippets/menu-items-title.html";
-var menuItemHtml = "snippets/menu-item.html";
+var categoryHtml        = "snippets/category-snippet.html";
+var menuItemsUrl        = "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+var menuItemsTitleHtml  = "snippets/menu-items-title.html";
+var menuItemHtml        = "snippets/menu-item.html";
 
 // Convenience function for inserting innerHTML for 'select'
 var insertHtml = function (selector, html) {
@@ -83,7 +81,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML,
+  //[...], // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -92,7 +91,6 @@ $ajaxUtils.sendGetRequest(
 // Builds HTML for the home page based on categories array
 // returned from the server.
 function buildAndShowHomeHTML (categories) {
-
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
@@ -101,16 +99,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
-        var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-          
-        var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName" , "'"+chosenCategoryShortName+ "'");
-
-    insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
-     },
-   false);
-    
-    
+      var chosenCategoryShortName = chooseRandomCategory(categories);
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -123,6 +112,11 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
+      var homeHtmlToInsertIntoMainPage = insertProperty(
+        homeHtml, 
+        "randomCategoryShortName", 
+        "'"+chosenCategoryShortName.short_name+"'"
+      );
       
 
 
@@ -130,9 +124,10 @@ function buildAndShowHomeHTML (categories) {
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
+      insertHtml("#main-content",homeHtmlToInsertIntoMainPage);
 
- 
-  // False here because we are getting just regular HTML from the server, so no need to process JSON.
+    },
+    false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
 
 
